d2dbd0057b2fb13a0f943179888af3b9b0003bc8
Akin-Net <akin@usp.br>
2014-05-02 22:56:27 -0300

    fix dependencies

--- api/lib/api/version.rb
+++ api/lib/api/version.rb
@@ -1,5 +1,5 @@
 module Pushar
   module Api
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
--- core/lib/core/version.rb
+++ core/lib/core/version.rb
@@ -1,5 +1,5 @@
 module Pushar
   module Core
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
