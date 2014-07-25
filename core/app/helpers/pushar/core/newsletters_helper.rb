7e86177d4bf0149bb2c7354a663803d1b3e1cd39
Akin-Net <akin@usp.br>
2014-07-25 18:54:29 -0300

    fix image alt on email open tracker

--- core/app/helpers/pushar/core/newsletters_helper.rb
+++ core/app/helpers/pushar/core/newsletters_helper.rb
@@ -5,7 +5,7 @@ module Pushar
     module NewslettersHelper
       def self.track(newsletter_id, email, host, tenant_id = nil)
         url = "#{host}/email/track/#{Base64.encode64("newsletter_id=#{newsletter_id}&email=#{email}&tenant_id=#{tenant_id}")}.png"
-        raw("<img src=\"#{url}\" alt="" width=\"1\" height=\"1\">")
+        raw("<img src=\"#{url}\" alt=\"\" width=\"1\" height=\"1\">")
       end
 
       def self.unsubscribe(token, text, host)
