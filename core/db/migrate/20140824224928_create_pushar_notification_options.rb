f1127e31d8c363c12735d4973fac45567a191e1c
Akin-Net <akin@usp.br>
2014-08-27 12:59:01 -0300

    fixed migration and versions

--- core/db/migrate/20140824224928_create_pushar_notification_options.rb
+++ core/db/migrate/20140824224928_create_pushar_notification_options.rb
@@ -8,8 +8,8 @@ class CreatePusharNotificationOptions < ActiveRecord::Migration
       t.timestamps
     end
 
-    remove_column :pushar_core_notification, :message
-    remove_column :pushar_core_notification, :options
+    remove_column :pushar_core_notifications, :message
+    remove_column :pushar_core_notifications, :options
 
     add_index :pushar_core_notification_options, :notification_id
   end
